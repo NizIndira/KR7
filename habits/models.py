88e64937890abd013959b9db9cf03bcadd642ee1
Indira <nizamutdinovaindira@gmail.com>
2024-04-26 01:26:11 +0300

    habit model added

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+
+class Habit(models.Model):
+
+    PERIODICITY_CHOICES = (
+        (0, 'Daily'),
+        (1, 'Every Monday'),
+        (2, 'Every Tuesday'),
+        (3, 'Every Wednesday'),
+        (4, 'Every Thursday'),
+        (5, 'Every Friday'),
+        (6, 'Every Saturday'),
+        (7, 'Every Sunday'),
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='пользователь',
+        **NULLABLE)
+    place = models.CharField(max_length=255, verbose_name='место', **NULLABLE)
+    time = models.TimeField(auto_now_add=True, verbose_name='время')
+    action = models.CharField(max_length=255, verbose_name='действие', **NULLABLE)
+    is_pleasant = models.BooleanField(default=False, verbose_name='признак приятной привычки')
+    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='связанная привычка', **NULLABLE)
+    periodicity = models.CharField(max_length=1, choices=PERIODICITY_CHOICES, default='0', verbose_name='периодичность')
+    reward = models.CharField(max_length=255, verbose_name='вознаграждение', **NULLABLE)
+    execute_time = models.PositiveSmallIntegerField(verbose_name='время на выполнение', **NULLABLE)
+    is_public = models.BooleanField(default=False, verbose_name='признак публичности')
+
+    def __str__(self):
+        return f'{self.owner} - {self.action}'
+
+    class Meta:
+        verbose_name = 'привычка'
+        verbose_name_plural = 'привычки'
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import HabitsPaginator
+from habits.permissions import IsOwner
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = HabitsPaginator
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+
+class HabitPublicListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = HabitsPaginator
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Habit.objects.filter(user=user)
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner]
